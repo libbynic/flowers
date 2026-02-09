7728b720738f42424703db9d938c2b1e9888fce5
libbynic <libbynic803@gmail.com>
2026-02-09 11:46:31 -0700

    fixing html tags in projects.jsx

--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 
-export function Projects() {
+export function Login() {
   return (
    <main>
-            <div class="center-div"/>
+        <body>
+            <div className="center-div"/>
                 <h1>Start New Project!</h1>
-                <div class="form-buttons"> 
-                    <button type="button" class="btn btn-outline-success">View Past Projects</button>
+                <div className="form-buttons"> 
+                    <button type="button" className="btn btn-outline-success">View Past Projects</button>
                 </div>
                 <br/>
                 <form action="formSubmit.html" method="post">
                     <li>
-                        <label for="text">Project Name:</label>
+                        <label htmlFor="text">Project Name:</label>
                         <input type="text" id="text" name="varText" placeholder="Name Here"/>
                     </li>
                 </form>
                 <form action="formSubmit.html" method="post">
-                    <div class="accordion" id="accordionPanelsStayOpenExample"/>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header">
-                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    <div className="accordion" id="accordionPanelsStayOpenExample"/>
+                        <div className="accordion-item">
+                            <h2 className="accordion-header">
+                                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
                                     <input type="text" id="text" name="varText" placeholder="Arrangement Name"/> 
                                 </button>
                             </h2>
-                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
-                                <div class="accordion-body">
-                                    <p>  <label for="Arrangement">What type of arrangement are you making? </label>
+                            <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show">
+                                <div className="accordion-body">
+                                    <p>  <label htmlFor="Arrangement">What type of arrangement are you making? </label>
                                         <select id="Arrangement" name="Arrangement">
                                             <option value="" disabled selected> Choose a design </option>
                                             <option value="Bouquet">Bouquet</option>
@@ -37,7 +38,7 @@ export function Projects() {
                                         </select>
                                     </p>
                                     <p>
-                                        <label for="flower type"> Flower:</label>
+                                        <label htmlFor="flower type"> Flower:</label>
             <select id="flower type" name="flower type">
                 <option value="" disabled selected> Choose a flower type </option>
                 <option value="flower"> Sun Flowers </option>
@@ -45,25 +46,25 @@ export function Projects() {
                 <option value="flower"> Daliah </option>
                 <option value="flower"> Chrysanthemums </option>
             </select>
-                <label for="number">Number of Stems:</label>
+                <label htmlFor="number">Number of Stems:</label>
                 <input type="number" name="varNumber" id="number" min="1" step="1" placeholder="0"/>
             <button>
-                <i class="bi bi-plus"></i>
+                <i className="bi bi-plus"></i>
             </button>
         </p>
     </div>
     </div>
   </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+  <div className="accordion-item">
+    <h2 className="accordion-header">
+        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
             <input type="text" id="text" name="varText" placeholder="Arrangement Name"/> 
         </button>
     </h2>
-    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
-      <div class="accordion-body">
+    <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse">
+      <div className="accordion-body">
       <p>
-          <label for="Arrangement">What type of arrangement are you making? </label>
+          <label htmlFor="Arrangement">What type of arrangement are you making? </label>
           <select id="Arrangement" name="Arrangement">
               <option value="" disabled selected> Choose a design </option>
               <option value="Bouquet">Bouquet</option>
@@ -75,7 +76,7 @@ export function Projects() {
             </select>
             </p>
             <p>
-            <label for="flower type"> Flower:</label>
+            <label htmlFor="flower type"> Flower:</label>
             <select id="flower type" name="flower type">
                 <option value="" disabled selected> Choose a flower type </option>
                 <option value="flower"> Sun Flowers </option>
@@ -83,24 +84,24 @@ export function Projects() {
                 <option value="flower"> Daliah </option>
                 <option value="flower"> Chrysanthemums </option>
             </select>
-                <label for="number">Number of Stems:</label>
+                <label htmlFor="number">Number of Stems:</label>
                 <input type="number" name="varNumber" id="number" min="1" step="1" placeholder="0"/>
              <button>
-                 <i class="bi bi-plus"></i>
+                 <i className="bi bi-plus"></i>
                 </button>
             </p>
         </div>
     </div>
-    <div class="accordion-item">
-        <h2 class="accordion-header">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+    <div className="accordion-item">
+        <h2 className="accordion-header">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
                 <input type="text" id="text" name="varText" placeholder="Arrangement Name"/> 
             </button>
         </h2>
-        <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
-      <div class="accordion-body">
+        <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse">
+      <div className="accordion-body">
         <p>
-            <label for="Arrangement">What type of arrangement are you making? </label>
+            <label htmlFor="Arrangement">What type of arrangement are you making? </label>
             <select id="Arrangement" name="Arrangement">
                 <option value="" disabled selected> Choose a design </option>
                 <option value="Bouquet">Bouquet</option>
@@ -112,7 +113,7 @@ export function Projects() {
             </select>
         </p>
         <p>
-            <label for="flower type"> Flower:</label>
+            <label htmlFor="flower type"> Flower:</label>
             <select id="flower type" name="flower type">
                 <option value="" disabled selected> Choose a flower type </option>
                 <option value="flower"> Sun Flowers </option>
@@ -120,22 +121,23 @@ export function Projects() {
                 <option value="flower"> Daliah </option>
                 <option value="flower"> Chrysanthemums </option>
             </select>
-            <label for="number">Number of Stems:</label>
+            <label htmlFor="number">Number of Stems:</label>
             <input type="number" name="varNumber" id="number" min="1" step="1" placeholder="0"/>
               <button>
-                <i class="bi bi-plus"></i>
+                <i className="bi bi-plus"></i>
             </button>
         </p>
       </div>
     </div>
 </div>
 </div>
-<div class="form-buttons">
-    <button class="btn btn-outline-success"> Add Arrangment </button>
+<div className="form-buttons">
+    <button className="btn btn-outline-success"> Add Arrangment </button>
 </div>
 </form>
 <br/>
 <img src="projectsImage.jpg" width="300" />
+</body>
 <br/>
 </main>
   );
