d2f97943139f257d496d4142cbe401639ae44e4b
libbynic <libbynic803@gmail.com>
2026-02-17 13:48:52 -0700

    work on create account button

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { registerUser, loginUser } from '../../service';
 
-export function Login() {
+export function Login({setUser}) {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState(' ')
+
+    function register(event) {
+        event.preventDefault();
+        registerUser(email, password);
+    }
+    function login(){
+        loginUser(email, password)
+    }
   return (
    <main>
 <iframe  src="https://www.youtube.com/embed/tgCm_fj9gvE?autoplay=1&mute=1&loop=1&playlist=tgCm_fj9gvE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> 
@@ -8,16 +19,16 @@ export function Login() {
     <h1>Welcome to Blossom Blueprint</h1>
 </div>
     <br/>
-    <form action="formSubmit.html" method="post" className="form">
+    <form onSubmit={register} className="form">
         <div className="form-group"> 
             <label htmlFor="email">Email: </label>
-            <input type="email" id="email" name="varEmail" />
+            <input type="email" id="email" name="varEmail" onChange={(e) => setEmail(e.target.value)} />
             <label htmlFor="password"> Password: </label>
-            <input type="password" id="password" name="varPassword" />
+            <input type="password" id="password" name="varPassword" onChange={(e) => setPassword(e.target.value)} />
         </div>
             <div className="form-buttons">
-            <button type="button" className="btn btn-outline-success">Log in</button>
-            <button type="button" className="btn btn-outline-success">Create Account</button>
+            <button type="button" className="btn btn-outline-success" onClick={login}>Log in</button>
+            <button type="submit" className="btn btn-outline-success">Create Account</button>
         </div>
     </form>
 <br/>
