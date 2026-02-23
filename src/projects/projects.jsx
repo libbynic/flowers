13523f55e1b8e5e4dd842111f6ad5acff51b734d
libbynic <libbynic803@gmail.com>
2026-02-23 10:53:39 -0700

    fixing accordion opening and closing

--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -16,13 +16,13 @@ export function Projects({user}) {
                     </li>
                 </form>
                 <form action="formSubmit.html" method="post">
-                    <Accordion>
-                        <Accordion.Item>
+                    <Accordion alwaysOpen>
+                        <Accordion.Item eventKey="0">
                         <Accordion.Header><input type="text" id="text" name="varText" placeholder="Arrangement Name"/> </Accordion.Header>
                         <Accordion.Body>
                                                         <p>  <label htmlFor="Arrangement">What type of arrangement are you making? </label>
-                                        <select id="Arrangement" name="Arrangement">
-                                            <option value="" disabled selected> Choose a design </option>
+                                        <select defaultValue="" id="Arrangement" name="Arrangement">
+                                            <option value="" disabled> Choose a design </option>
                                             <option value="Bouquet">Bouquet</option>
                                             <option value="Arch"> Arch </option>
                                             <option value="Corsage"> Corsage </option> 
@@ -48,20 +48,12 @@ export function Projects({user}) {
         </p>
                         </Accordion.Body>
                      </Accordion.Item>
-                    </Accordion>
-
-                    <div className="accordion" id="accordionPanelsStayOpenExample">
-                        <div className="accordion-item">
-                            <h2 className="accordion-header">
-                                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                                    <input type="text" id="text" name="varText" placeholder="Arrangement Name"/> 
-                                </button>
-                            </h2>
-                            <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show">
-                                <div className="accordion-body">
-                                    <p>  <label htmlFor="Arrangement">What type of arrangement are you making? </label>
-                                        <select id="Arrangement" name="Arrangement">
-                                            <option value="" disabled selected> Choose a design </option>
+                  <Accordion.Item eventKey="1">
+                        <Accordion.Header><input type="text" id="text" name="varText" placeholder="Arrangement Name"/> </Accordion.Header>
+                        <Accordion.Body>
+                                                        <p>  <label htmlFor="Arrangement">What type of arrangement are you making? </label>
+                                        <select defaultValue="" id="Arrangement" name="Arrangement">
+                                            <option value="" disabled> Choose a design </option>
                                             <option value="Bouquet">Bouquet</option>
                                             <option value="Arch"> Arch </option>
                                             <option value="Corsage"> Corsage </option> 
@@ -85,32 +77,24 @@ export function Projects({user}) {
                 <i className="bi bi-plus"></i>
             </button>
         </p>
-    </div>
-    </div>
-  </div>
- 
-  <div className="accordion-item">
-    <h2 className="accordion-header">
-        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-            <input type="text" id="text" name="varText" placeholder="Arrangement Name"/> 
-        </button>
-    </h2>
-    <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse">
-      <div className="accordion-body">
-      <p>
-          <label htmlFor="Arrangement">What type of arrangement are you making? </label>
-          <select id="Arrangement" name="Arrangement">
-              <option value="" disabled selected> Choose a design </option>
-              <option value="Bouquet">Bouquet</option>
-                <option value="Arch"> Arch </option>
-                <option value="Corsage"> Corsage </option> 
-                <option value="Boutonnieres"> Boutonniere </option>
-                <option value="Bud Vases"> Bud Vase </option>
-                <option value=" Custom "> Custom </option>
-            </select>
-            </p>
-            <p>
-            <label htmlFor="flower type"> Flower:</label>
+                        </Accordion.Body>
+                     </Accordion.Item>
+                  <Accordion.Item eventKey="2">
+                        <Accordion.Header><input type="text" id="text" name="varText" placeholder="Arrangement Name"/> </Accordion.Header>
+                        <Accordion.Body>
+                                                        <p>  <label htmlFor="Arrangement">What type of arrangement are you making? </label>
+                                        <select defaultValue="" id="Arrangement" name="Arrangement">
+                                            <option value="" disabled> Choose a design </option>
+                                            <option value="Bouquet">Bouquet</option>
+                                            <option value="Arch"> Arch </option>
+                                            <option value="Corsage"> Corsage </option> 
+                                            <option value="Boutonnieres"> Boutonniere </option>
+                                            <option value="Bud Vases"> Bud Vase </option>
+                                            <option value=" Custom "> Custom </option>
+                                        </select>
+                                    </p>
+                                    <p>
+                                        <label htmlFor="flower type"> Flower:</label>
             <select id="flower type" name="flower type">
                 <option value="" disabled selected> Choose a flower type </option>
                 <option value="flower"> Sun Flowers </option>
@@ -120,61 +104,19 @@ export function Projects({user}) {
             </select>
                 <label htmlFor="number">Number of Stems:</label>
                 <input type="number" name="varNumber" id="number" min="1" step="1" placeholder="0"/>
-             <button>
-                 <i className="bi bi-plus"></i>
-                </button>
-            </p>
-        </div>
-    </div>
-    </div>
-
-    <div className="accordion-item">
-        <h2 className="accordion-header">
-            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                <input type="text" id="text" name="varText" placeholder="Arrangement Name"/> 
-            </button>
-        </h2>
-        <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse">
-      <div className="accordion-body">
-        <p>
-            <label htmlFor="Arrangement">What type of arrangement are you making? </label>
-            <select id="Arrangement" name="Arrangement">
-                <option value="" disabled selected> Choose a design </option>
-                <option value="Bouquet">Bouquet</option>
-                <option value="Arch"> Arch </option>
-                <option value="Corsage"> Corsage </option> 
-                <option value="Boutonnieres"> Boutonniere </option>
-                <option value="Bud Vases"> Bud Vase </option>
-                <option value=" Custom "> Custom </option>
-            </select>
-        </p>
-        <p>
-            <label htmlFor="flower type"> Flower:</label>
-            <select id="flower type" name="flower type">
-                <option value="" disabled selected> Choose a flower type </option>
-                <option value="flower"> Sun Flowers </option>
-                <option value="flower"> Roses </option>
-                <option value="flower"> Daliah </option>
-                <option value="flower"> Chrysanthemums </option>
-            </select>
-            <label htmlFor="number">Number of Stems:</label>
-            <input type="number" name="varNumber" id="number" min="1" step="1" placeholder="0"/>
-              <button>
+            <button>
                 <i className="bi bi-plus"></i>
             </button>
         </p>
-      </div>
-    </div>
-</div>
-</div>
-<div className="form-buttons">
+                        </Accordion.Body>
+                     </Accordion.Item>
+                </Accordion>
     <button className="btn btn-outline-success"> Add Arrangment </button>
-</div>
 </form>
+</div>   
 <br/>
 <div className='center-img'>
 <img src="projectsImage.jpg" width="300" />
-</div>
 </div>
 <br/>
 </main>
