65bf37675de1fa69d629155f0c69a0dff573a084
libbynic <libbynic803@gmail.com>
2026-02-25 14:24:07 -0700

    1st attempt at chat js

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './chat.css'; 
 export function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+
+  const send = () => {
+    if (input.trim() === "") return; 
+    const userMessage = { text: input, sender: "user"}; 
+    const response = {text:"Hey dawg", sender:"default"}
+    setMessages([...messages, userMessage, response])
+    setInput("");
+  }
   return (
+
   <main>
     <div className="chat-box"> 
       <div className="messages">
@@ -9,8 +20,8 @@ export function Chat() {
           <div className="message-user">Hi!</div>
       </div>
       <div className="input-box"> 
-        <input type="text" id="text" name="varText" placeholder="Start Typing..."/> 
-        <button> Send </button>
+        <input type="text" id="text" name="varText" placeholder="Start Typing..." value={input} onChange={(e) => setInput(e.target.value)}/> 
+        <button onClick={send}> Send </button>
         </div> 
     </div> 
         </main>
