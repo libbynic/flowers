cf72c53efcc73f621bad86204f3e8ea5cf4eef39
libbynic <libbynic803@gmail.com>
2026-03-12 15:45:09 -0600

    add 3rd party api

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -5,13 +5,20 @@ export function About() {
   
   const [img, setImage] = React.useState();
   React.useEffect(() => {
-    setImage(getPicture());
+    fetch("https://www.sourcesplash.com/i/random?q=flowers")
+      .then((res) => {
+        // res.url contains the final image URL after redirect
+        setImage(res.url);
+      })
+      .catch((err) => {
+        console.error("Error fetching floral image:", err);
+      });
   }, []);
   return (
     <div>
      <main>
             <div className="center-img"> 
-              <img src={img} width={250}/> 
+              <img src={img} width={250} alt="Random floral"/> 
             </div>
             <p className="box-text">
             The idea for Blossom Blueprint came from my sister in law who is a florist. She found herself wasting a lot of time organizing her floral recipes, searching wholesaler websites in order to find good prices, and trying to calculate order details. The purpose of this tool is to facilitate the organization of the design details for every florist. With this website, a florist will be able to plan out their designs, see prices, calculate client markups and save time during their design process.
